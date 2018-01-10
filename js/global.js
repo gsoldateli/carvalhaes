1609b47f1f009e1f56f261000133845d44261661
Guilherme Soldateli <guilherm3soldateli@gmail.com>
2018-01-10 08:23:56 +0000

    Splash screen

--- js/global.js
+++ js/global.js
@@ -1,56 +1,106 @@
 (function(window){
 
-$contentHolder = document.querySelector('.content-holder');
+	$contentHolder = document.querySelector('.content-holder');
 
-/* Registra ação do menu lateral */
-$waypoints = document.querySelectorAll('[data-menu-id]');
-var $pages = document.querySelectorAll('.page');
-console.log($pages);
+	/* Registra ação do menu lateral */
+	$waypoints = document.querySelectorAll('[data-menu-id]');
+	var $pages = document.querySelectorAll('.page');
+	console.log($pages);
 
-//Adiciona ação de click nos menus laterais para mostrar a página
-for(var x=0; x<$waypoints.length; x++) {
-	var $wp = $waypoints[x];
-	$wp.addEventListener('click',goTo(x));
-}
-
-function goTo(index) {
-	return function(){
-		scrollToElement($pages[index]);
-	};
-}
-
-//Registra scroll que ativa menu lateral quando chega em determinada seção
-$contentHolder.addEventListener('scroll',function(){
-	for(var x=0; x<$pages.length;x++) {
-		var $page = $pages[x];
-		if(isInViewport($page,-30)) {
-			$waypoints[x].classList.add('menu__item--active');
-			$page.classList.add('page--active');
-		}
-		else {
-			$page.classList.remove('page--active');
-			$waypoints[x].classList.remove('menu__item--active');
-		}
-	}
-	console.log('Opa');
-});
-
-function isInViewport($el,threshold) {
-	
-	if(threshold === undefined) {
-		threshold = 0;
+	//Adiciona ação de click nos menus laterais para mostrar a página
+	for(var x=0; x<$waypoints.length; x++) {
+		var $wp = $waypoints[x];
+		$wp.addEventListener('click',goTo(x));
 	}
 
-    var rect = $el.getBoundingClientRect();
-    var elemTop = rect.top;
-    var elemBottom = rect.bottom;
+	function goTo(index) {
+		return function(){
+			scrollToElement($pages[index]);
+		};
+	}
+
+	//Registra scroll que ativa menu lateral quando chega em determinada seção
+	$contentHolder.addEventListener('scroll',function(){
+		for(var x=0; x<$pages.length;x++) {
+			var $page = $pages[x];
+			if(isInViewport($page,-30)) {
+				$waypoints[x].classList.add('menu__item--active');
+				$page.classList.add('page--active');
+			}
+			else {
+				$page.classList.remove('page--active');
+				$waypoints[x].classList.remove('menu__item--active');
+			}
+		}
+	});
+
+	function isInViewport($el,threshold) {
+		
+		if(threshold === undefined) {
+			threshold = 0;
+		}
+
+	    var rect = $el.getBoundingClientRect();
+	    var elemTop = rect.top;
+	    var elemBottom = rect.bottom;
 
 
-    // Only completely visible elements return true:
-    var isVisible = (elemTop < -threshold && elemBottom > 0); 
+	    // Only completely visible elements return true:
+	    var isVisible = (elemTop < -threshold && elemBottom > 0); 
 
 
-    return isVisible;
-}
+	    return isVisible;
+	}
+
+	//Função página carregada.
+	window.$ = {};
+	$.ready = function(fn) {
+	  if (document.readyState == "complete")
+	      return fn();
+
+	  if (window.addEventListener)
+	      window.addEventListener("load", fn, false);
+	  else if (window.attachEvent)
+	      window.attachEvent("onload", fn);
+	  else
+	      window.onload = fn;
+	}
+
+	window.fadeOut = function(el){
+	  el.style.opacity = 1;
+
+	  (function fade() {
+	    if ((el.style.opacity -= .1) < 0) {
+	      //el.remove();
+	      el.style.display = "none";
+	    } else {
+	      requestAnimationFrame(fade);
+	    }
+	  })();
+	}
+
+	window.fadeIn = function(el){
+	  el.style.opacity = 0;
+	  el.style.display = "block";
+
+	  (function fade() {
+	    var val = parseFloat(el.style.opacity);
+	    //Increase opacity while it is not totally opaque.
+	    if (!((val += .1) > 1)) {
+	      el.style.opacity = val;
+	      requestAnimationFrame(fade);
+	    }
+	  })();
+	}
+
 
 })(window);
+
+
+function onLoadPage() {
+
+	//Remove splash screen
+	fadeOut(document.querySelector('.splash'));
+}
+
+$.ready(onLoadPage)
