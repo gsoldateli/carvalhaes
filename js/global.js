36f95ed9ea5828d9128caa68caac9a2212ef7bed
Guilherme Soldateli <guilherm3soldateli@gmail.com>
2018-01-10 08:28:02 +0000

    Ajuste legibilidade código

--- js/global.js
+++ js/global.js
@@ -1,39 +1,39 @@
 (function(window){
 
-	$contentHolder = document.querySelector('.content-holder');
+	window.registerMenuActions = function registerMenuActions() {
+		$contentHolder = document.querySelector('.content-holder');
 
-	/* Registra ação do menu lateral */
-	$waypoints = document.querySelectorAll('[data-menu-id]');
-	var $pages = document.querySelectorAll('.page');
-	console.log($pages);
+		/* Registra ação do menu lateral */
+		$waypoints = document.querySelectorAll('[data-menu-id]');
+		var $pages = document.querySelectorAll('.page');
 
-	//Adiciona ação de click nos menus laterais para mostrar a página
-	for(var x=0; x<$waypoints.length; x++) {
-		var $wp = $waypoints[x];
-		$wp.addEventListener('click',goTo(x));
-	}
-
-	function goTo(index) {
-		return function(){
-			scrollToElement($pages[index]);
-		};
-	}
-
-	//Registra scroll que ativa menu lateral quando chega em determinada seção
-	$contentHolder.addEventListener('scroll',function(){
-		for(var x=0; x<$pages.length;x++) {
-			var $page = $pages[x];
-			if(isInViewport($page,-30)) {
-				$waypoints[x].classList.add('menu__item--active');
-				$page.classList.add('page--active');
-			}
-			else {
-				$page.classList.remove('page--active');
-				$waypoints[x].classList.remove('menu__item--active');
-			}
+		//Adiciona ação de click nos menus laterais para mostrar a página
+		for(var x=0; x<$waypoints.length; x++) {
+			var $wp = $waypoints[x];
+			$wp.addEventListener('click',goTo(x));
 		}
-	});
 
+		function goTo(index) {
+			return function(){
+				scrollToElement($pages[index]);
+			};
+		}
+
+		//Registra scroll que ativa menu lateral quando chega em determinada seção
+		$contentHolder.addEventListener('scroll',function(){
+			for(var x=0; x<$pages.length;x++) {
+				var $page = $pages[x];
+				if(isInViewport($page,-30)) {
+					$waypoints[x].classList.add('menu__item--active');
+					$page.classList.add('page--active');
+				}
+				else {
+					$page.classList.remove('page--active');
+					$waypoints[x].classList.remove('menu__item--active');
+				}
+			}
+		});
+	}
 	function isInViewport($el,threshold) {
 		
 		if(threshold === undefined) {
@@ -98,9 +98,10 @@
 
 
 function onLoadPage() {
-
 	//Remove splash screen
 	fadeOut(document.querySelector('.splash'));
 }
 
-$.ready(onLoadPage)
+$.ready(onLoadPage);
+
+registerMenuActions();
